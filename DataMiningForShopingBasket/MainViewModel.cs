3b496e1baa5175926fa8e08e81680d610204fe47
Николай Колготин <kolgotin@bk.ru>
2020-05-21 23:52:06 +0400

    Создан проет с окном авторизации

---
+++ DataMiningForShopingBasket/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DataMiningForShopingBasket.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DataMiningForShopingBasket
+{
+    public class MainViewModel
+    {
+        public object CurrentUC { get; set; } = new AuthorizationView();
+    }
+}
---
+++ DataMiningForShopingBasket/ViewModels/AuthorizationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMiningForShopingBasket.ViewModels
+{
+    public class AuthorizationViewModel
+    {
+        public string Login { get; set; } = "qqqaaaaaaa";
+        public string Password { get; set; }
+    }
+}
