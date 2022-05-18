352b6bedf0424b18a35fe3fbd86467a0542c5eb8
Nikolay Kolgotin <nikolay.kolgotin@rt.ru>
2022-05-19 00:57:55 +0300

    проект переименован в DataMiningForShoppingBasket

---
+++ DataMiningForShoppingBasket/Events/IAsyncCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DataMiningForShoppingBasket.Events
+{
+    interface IAsyncCommand : ICommand
+    {
+        bool IsActive { get; set; }
+
+        event EventHandler IsActiveChanged;
+
+        /// <summary>
+        /// Команда отмены для выполняемой команды.
+        /// </summary>
+        ICommand CancelCommand { get; }
+    }
+}
