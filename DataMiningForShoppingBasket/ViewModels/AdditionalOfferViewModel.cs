2a42de7e71f09e8c3fb15072b1cfc6721ca8f988
Kolgotin <56718691+Kolgotin@users.noreply.github.com>
2023-04-16 06:06:30 +0300

    исправлен интерфейс IPrepareOfferHandler и реализация (#22)
    в интерфейс IPrepareOfferHandler и в реализацию сигнатура метода PrepareOffer изменена - возвращает словарь с ключём Products и значением decimal - уверенность рекомендации;
    мелкий рефакторинг
    
    Co-authored-by: Nikolay Kolgotin <nikolay.kolgotin@rt.ru>

---
+++ DataMiningForShoppingBasket/ViewModels/AdditionalOfferViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DynamicData.Binding;
+
+namespace DataMiningForShoppingBasket.ViewModels
+{
+    public class AdditionalOfferViewModel : AbstractNotifyPropertyChanged
+    {
+        private decimal _confidence;
+        public Products Product { get; }
+
+        public decimal Confidence
+        {
+            get => _confidence;
+            set => SetAndRaise(ref _confidence, value);
+        }
+
+        public AdditionalOfferViewModel(Products product, decimal confidence)
+        {
+            Product = product;
+            Confidence = confidence;
+        }
+    }
+}
